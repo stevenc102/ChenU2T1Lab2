33a67af54aeb2c152167e024caf929b00165ada1
1E10laptop <sample@bths.edu>
2022-09-28 11:52:26 -0400

    initial commit

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class CircleRunner {
+public class CircleRunner {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(5.0);
+        // A. create a Circle object with radius 5.0
+        circle1.printArea();
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+
+        // C. BEFORE running this code, PREDICT what gets printed
+
+        // D. run the code and confirm your prediction! Were you correct?
+    }
 }
